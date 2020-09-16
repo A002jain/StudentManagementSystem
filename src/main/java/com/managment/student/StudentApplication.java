77189eb9c359b69747d3fafafd696cb014b58bd3
Kimesh Gupta <kimesh_gupta@infoobjects.com>
2020-09-16 14:52:00 +0530

    added validation

--- src/main/java/com/managment/student/StudentApplication.java
+++ src/main/java/com/managment/student/StudentApplication.java
@@ -2,7 +2,9 @@ package com.managment.student;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class StudentApplication {
 	public static void main(String[] args) {
